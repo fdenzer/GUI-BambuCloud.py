097ed98525aea65c372d74ea5308489e77728ad4
Frank Denzer <frank.denzer@ard.de>
2025-06-30 16:29:42 +0200

    checks existing credentials first

--- auto_install+runGUI.sh
+++ auto_install+runGUI.sh
@@ -2,7 +2,7 @@ python3 -m venv .venv
 source .venv/bin/activate
 python3 -m pip install -r requirements.txt
 
-if [ -f .env.example ]; then
+if [ -f .env.example ] && [ ! -s .env ]; then
   cp .env.example .env
   open -W .env
 fi
